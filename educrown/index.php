ef17745323984602f270d6d7a6efd085e5afde01
akhil kishor <b.akhil072@gmail.com>
2020-04-17 18:49:45 +0530

    new commitse

--- educrown/index.php
+++ educrown/index.php
@@ -271,7 +271,7 @@
             <div class="row">
                 <div class="col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2 col-sm-12 col-xs-12">
                     <div class="area-title text-center wow fadeIn">
-                        <h2 class="xs-font26">Our Trending Courses</h2>
+                        <h2 class="xs-font26">Our Courses</h2>
                     </div>
                 </div>
             </div>
